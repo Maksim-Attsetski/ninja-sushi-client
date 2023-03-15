f9fccd4fada2b60d0c241474e045c574703ec1d7
Maksim Attsetski <maks2001maks2001@gmail.com>
2023-03-15 22:19:26 +0300

    Revert "add user types"
    This reverts commit df43b23d7de6c8fcea8aaf0893e11885aa088642.

--- src/models/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './user';
-export * from './ingredient';
-export * from './product';
--- src/models/ingredient.ts
+++
@@ -1,6 +0,0 @@
-export interface IIngredient {
-  _id: string;
-  name: string;
-  image: string;
-  createAt: number;
-}
